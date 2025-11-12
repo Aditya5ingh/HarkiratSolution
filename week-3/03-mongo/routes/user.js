c725b1f67875c92724f3b2311835731712579ba4
Aditya <aditya.singh@rnf.tech>
2025-11-12 18:48:29 +0530

    third comit

--- week-3/03-mongo/routes/user.js
+++ week-3/03-mongo/routes/user.js
@@ -1,7 +1,7 @@
 const { Router } = require("express");
 const router = Router();
 const userMiddleware = require("../middleware/user");
-const { User } = require("../db");
+const { User, Course } = require("../db");
 
 // User Routes
 router.post('/signup', async(req, res) => {
@@ -18,10 +18,30 @@ router.get('/courses', (req, res) => {
     Course.find().then(function(courses){
             res.json({ courses: courses });
         });
+        
 });
 
 router.post('/courses/:courseId', userMiddleware, (req, res) => {
     // Implement course purchase logic
+    const courseId= req.params.courseId;
+    const username=req.headers.username;
+    const password=req.headers.password;
+
+    // User.findOne({ username: username, password: password }).then(function(user){
+    //     if(user){
+    //         if(!user.purchasedCourses){
+    //             user.purchasedCourses=[];
+    //         }
+            
+    //         user.purchasedCourses.push(Course[courseId]);
+    //         user.save().then(function(){
+    //             res.json( { message: 'Course purchased successfully' } )
+    //         });
+    //     } else {
+    //         res.status(403).json( { message: 'User not found' } )
+    //     }
+    // });
+    User.updateOne({ username: username, password: password }, { $purchasedCourses: { "$push": courseId } });
 });
 
 router.get('/purchasedCourses', userMiddleware, (req, res) => {
