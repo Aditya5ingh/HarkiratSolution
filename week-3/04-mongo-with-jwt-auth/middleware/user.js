15758d2b79e0725ddf69ad1166eeca398ba2744c
Aditya <aditya.singh@rnf.tech>
2025-11-14 18:36:52 +0530

    3.4commit 1

--- week-3/04-mongo-with-jwt-auth/middleware/user.js
+++ week-3/04-mongo-with-jwt-auth/middleware/user.js
@@ -1,6 +1,24 @@
+const jwt = require('jsonwebtoken');
+const JWT_SECRET = require('..');
 function userMiddleware(req, res, next) {
     // Implement user auth logic
     // You need to check the headers and validate the user from the user DB. Check readme for the exact headers to be expected
+
+    const token = req.headers.Authorization;
+
+    const words = token.split(' ');
+    const jwtToken = words[1];
+
+    // Validate the token and extract admin details
+    // If valid, call next()
+    // If invalid, respond with 401 Unauthorized
+    const decodedValue = jwt.verify(jwtToken, JWT_SECRET);
+    if (decodedValue.username) {
+        next();
+    } else
+        res.status(403).json({
+            msg: "You are not authenticated."
+        })
 }
 
 module.exports = userMiddleware;
